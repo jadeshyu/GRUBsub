3931cba94aa4b710b207711dec6463e0ee62b171
JadeShyu <jadeshyu@gmail.com>
2017-12-03 13:10:23 -0800

    Stable. Food table working, data passed over

--- grubSub/ViewController.swift
+++ grubSub/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    
+    @IBOutlet var table: UITableView!
+
+    var food: [String] = []
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-        return 1
+        return food.count
         
     }
     
@@ -22,7 +27,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        //cell.textLabel?.text = ""
+        print(indexPath)
+        
+        cell.textLabel?.text = food[indexPath[1]]
         
         return cell
         
@@ -33,7 +40,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let foodObject = UserDefaults.standard.object(forKey: "foods")
+        
+        print("Tis is itemsObject")
+        print(foodObject)
+        
+        //add on existing to array
+        if let tempItems = foodObject as? [String] {
+            
+            food = tempItems
+            
+            print("Tis is tempItem")
+            print(tempItems)
+        }
+        
+        table.reloadData()
+        
+        print("bakkk first page")
+        print(food)
+    }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
