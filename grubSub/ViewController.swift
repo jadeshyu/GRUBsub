694c70ac2e6e4b8f329aa2ce96b1206d37809a34
JadeShyu <jadeshyu@gmail.com>
2017-12-02 19:29:18 -0800

    Table view  explorations

--- grubSub/ViewController.swift
+++ grubSub/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return 1
+        
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        //cell.textLabel?.text = ""
+        
+        return cell
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
